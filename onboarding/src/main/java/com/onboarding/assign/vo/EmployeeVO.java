4d43bc5cf060474c1dc8d6b0dd31418e8b300676
PARIMANAM <muruganand.parimanam@capgemini.com>
2022-04-19 18:08:11 +0530

    Changes for the mpastruct codes

--- onboarding/src/main/java/com/onboarding/assign/vo/EmployeeVO.java
+++ onboarding/src/main/java/com/onboarding/assign/vo/EmployeeVO.java
@@ -1,14 +1,42 @@
 package com.onboarding.assign.vo;
-
 import lombok.Data;
 
 @Data
-public class EmployeeVO {	
-	
+public class EmployeeVO {		
 	private long id;
 	private String empCode;
 	private String name;
 	private String designation;
 	private String department;
+	/*public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getEmpCode() {
+		return empCode;
+	}
+	public void setEmpCode(String empCode) {
+		this.empCode = empCode;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}*/
 
 }
